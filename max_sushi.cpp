9f8400ae7a5575a0a60ca8ddbfee6421e285b0c2
Suriya Sureshkumar <240171.ad@rmkec.ac.in>
2026-09-02 14:09:19 +0530

    Migrate max_sushi from Java to C++
    Converted Java implementation to C++ and updated input/output methods.

--- max_sushi.cpp
+++ max_sushi.cpp
@@ -1,31 +1,39 @@
-import java.util.*;
-class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] a = new int[n];
-        int[] b = new int[m];
-        for(int i=0;i<n;i++){
-            a[i]=sc.nextInt();
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> a(n);
+    vector<int> b(m);
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    for (int j = 0; j < m; j++) {
+        cin >> b[j];
+    }
+    
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    
+    int c = 0;
+    int n1 = a.size() - 1;
+    int m1 = b.size() - 1;
+    
+    while (n1 >= 0 && m1 >= 0) {
+        if (a[n1] * 2 >= b[m1]) {
+            c++;
+            n1--;
+            m1--;
+        } else {
+            m1--;
         }
-        for(int j=0;j<m;j++){
-            b[j]=sc.nextInt();
-        }
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int c=0;
-        int n1=a.length-1;
-        int m1=b.length-1;
-        while (n1!=0 && m1!=0){
-            if(a[n1]*2>=b[m1]){
-                c+=1;
-                n1-=1;
-                m1-=1;
-            }
-            else{
-                m1-=1;
-            }
-        } 
-        System.out.println(c);    }
+    }
+    
+    cout << c << endl;
+    
+    return 0;
 }
